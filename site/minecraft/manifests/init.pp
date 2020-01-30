10d98bbc4349091df367dde2e02d08faff633115
Sambhav Kalia <sambhavkalia@gmail.com>
2020-01-29 22:13:33 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,24 +1,24 @@
 class minecraft {
-  file {'/opt/minecraft':
+  file {‘/opt/minecraft’:
     ensure => directory,
-   }
-   file {'/opt/minecraft/minecraft_server.jar':
+  }
+  file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
-   }
-   package {'java':
+    source      => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+  }
+  package {‘java’:
     ensure => present,
-   }
-   file {'/opt/minecraft/eula.txt':
-     ensure => file,
-     content => 'eula=true',
-   }
-   file {'/etc/systemd/system/minecraft.service':
+  }
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
-   }
-   service {'minecraft':
-    enable => running,
-    ensure => true,
-   }
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
+    ensure => file,
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
+    ensure => running,
+    enable => true,
+  }
 }
