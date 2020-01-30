fbb82269155b1a12ab1c62e51980bf530f405232
Sambhav Kalia <sambhavkalia@gmail.com>
2020-01-29 22:39:03 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,7 +5,7 @@ class minecraft {
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
-    before => Service[‘minecraft’],
+    before => Service['minecraft'],
   }
 
 
