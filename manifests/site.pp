1db768a7b615c8012e5621308de36da6b2754332
Sambhav Kalia <sambhavkalia@gmail.com>
2020-01-29 22:10:22 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,24 @@
 node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
-    file { '/root/README':
-    ensure  => file,
-    content => "Welcome to ${fqdn}\n",
-   }
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
+  }
 }
-node 'mintest.puppet.vm' {
+node 'mintest.puppet.vm': {
   include role::minecraft_server
- }
-node /^web/ {
-    include role::app_server
 }
-
+node /^web/ { 
+  include role::app_server
+}
 node /^db/ {
-    include role::db_server
+  include role::db_server
 }
