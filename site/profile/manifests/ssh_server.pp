7f8d6357dddc70e223e66e22f5bd113a0fe16765
Sambhav Kalia <sambhavkalia@gmail.com>
2020-01-29 22:17:07 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,15 +1,15 @@
 class profile::ssh_server {
-        package {'openssh-server':
-                ensure => present,
-        }
-        service { 'sshd':
-                ensure => 'running',
-                enable => 'true',
-        }
-        ssh_authorized_key { 'root@master.puppet.vm':
-                ensure => present,
-                user   => 'root',
-                type   => 'ssh-rsa',
-                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQJw6WjA2MRqP3yg0bg1haWSdZelL5bbw7FuDKGaywUheUI3Py8pACQOBZzvXuWNHtmhteVuAYCouDO1AmPxzSABOYvPIp99pOIFaUhFHt8qnahk16E9o0i6LvCjFWvuUsxXLryqiQRAazYAb7YsfyrM98zDqZ2aS3hwal1/zYkHX54cB1S+2f2HkNB10qpwEMPWZ14/RzlvT1rDn5Aw0e7XQ2YVMZOyXqOGhWAZWYWLur5zxwDeLgNQLxU5M4rnJ/G+mB56GwZeGS/rDxpKhV+VYDq/TKnZaSpZCnwUHjSQJmy9Prx9swU3Qb8ShYKIs5hdHyB/LwMbtvDBUFvDpp',
-        }
+  package {'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    ensure => 'running',
+    enable => 'true',
+  }
+  ssh_authorized_key { 'root@master.puppet.vm':
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQJw6WjA2MRqP3yg0bg1haWSdZelL5bbw7FuDKGaywUheUI3Py8pACQOBZzvXuWNHtmhteVuAYCouDO1AmPxzSABOYvPIp99pOIFaUhFHt8qnahk16E9o0i6LvCjFWvuUsxXLryqiQRAazYAb7YsfyrM98zDqZ2aS3hwal1/zYkHX54cB1S+2f2HkNB10qpwEMPWZ14/RzlvT1rDn5Aw0e7XQ2YVMZOyXqOGhWAZWYWLur5zxwDeLgNQLxU5M4rnJ/G+mB56GwZeGS/rDxpKhV+VYDq/TKnZaSpZCnwUHjSQJmy9Prx9swU3Qb8ShYKIs5hdHyB/LwMbtvDBUFvDpp',
+  }  
 }
